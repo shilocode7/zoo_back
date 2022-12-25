e7194b037e1d57e64ae402fe2f1954f49e79145c
shilo <shilonir123@gmail.com>
2022-12-25 13:21:14 +0200

    first commit

--- app.py
+++ app.py
@@ -1,64 +1,178 @@
-from flask import Flask,request
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 import json
-from flask_cors import CORS,cross_origin
+from flask_cors import CORS, cross_origin
+from datetime import datetime, date, time, timezone
 
 app = Flask(__name__)
 CORS(app)
 
- 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Zoo_Animals.sqlite3'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///libery.DB.sqlite3'
 app.config['SECRET_KEY'] = "random string"
- 
 db = SQLAlchemy(app)
- 
+
 # model
-class Animals(db.Model):
-    id = db.Column('animals_id', db.Integer, primary_key = True)
-    animal = db.Column(db.String(100))
-    kind = db.Column(db.String(50))
-    size = db.Column(db.String(50))
- 
-    def __init__(self, animal, kind, size):
-        self.animal = animal
-        self.kind = kind
-        self.size = size
-# model
-@app.route('/',methods=["POST","GET"])
-@app.route('/<aid>',methods=["DELETE","PUT"])
+######################## ---BOOKS CLASS---  #############################################
+
+class Books(db.Model):
+    id = db.Column('book_id', db.Integer, primary_key=True)
+    book_name = db.Column(db.String)
+    author = db.Column(db.String)
+    year_published = db.Column(db.Integer)
+    book_type = db.Column(db.Integer)  # 1/2/3
+    books = db.relationship('Loans', backref='books')
+
+    def __init__(self, book_name, author, year_published, book_type):
+        self.book_name = book_name
+        self.author = author
+        self.year_published = year_published
+        self.book_type = book_type
+
+######################## ---CUSTOMER CLASS---  #############################################
+
+
+class Customers(db.Model):
+    id = db.Column('customer_id', db.Integer, primary_key=True)
+    customer_name = db.Column(db.String)
+    City = db.Column(db.String)
+    Age = db.Column(db.Integer)
+    customer_mode = db.Column(db.String)
+    customers = db.relationship('Loans', backref='customers')
+
+    def __init__(self, customer_name, City, Age, customer_mode):
+        self.customer_name = customer_name
+        self.City = City
+        self.Age = Age
+        self.customer_mode = customer_mode
+
+######################## ---LOANS CLASS---  #############################################
+
+
+class Loans(db.Model):
+    id = db.Column("loan_id", db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.customer_id'))
+    book_id = db.Column(db.Integer, db.ForeignKey('books.book_id'))
+    Loan_date = db.Column(db.String)
+    Return_date = db.Column(db.String)
+    loan_moed = db.Column(db.String)
+
+    def __init__(self, customer_id, book_id, Loan_date, Return_date,loan_moed):
+        self.customer_id = customer_id
+        self.book_id = book_id
+        self.Loan_date = Loan_date
+        self.Return_date = Return_date
+        self.loan_moed = loan_moed
+
+
+
+######################## ---BOOKS CRUDE---  #############################################
+
+@app.route('/books', methods=["POST", "GET"])
+@app.route('/books/<bid>', methods=["DELETE", "PUT"])
 @cross_origin()
-def crude_zoo(aid=0): 
+def crude_book(bid=0):
     if request.method == 'POST':
         request_data = request.get_json()
-        animal = request_data['animal']
-        kind= request_data["kind"]
-        size= request_data["size"]
-        newAnimal= Animals(animal, kind, size)
-        db.session.add (newAnimal)
+        book_name = request_data['book_name']
+        author = request_data["author"]
+        year_published = request_data["year_published"]
+        book_type = request_data["book_type"]
+        newBook = Books(book_name, author, year_published, book_type)
+        db.session.add(newBook)
         db.session.commit()
-        return "A new animal was addad to zoo"
+        return {"book": "created"}
     if request.method == 'GET':
-        res=[]
-        for beast in Animals.query.all():
-            res.append({"animal":beast.animal,"id":beast.id,"kind":beast.kind, "size":beast.size})
-        return  (json.dumps(res))
-    if request.method == 'DELETE': #not implemented yet
-        del_animal = Animals.query.filter_by(id = aid).first()
-        db.session.delete(del_animal)
+        res = []
+        for book in Books.query.all():
+            res.append({"book_name": book.book_name, "id": book.id, "author": book.author,
+                       "year_published": book.year_published, "book_type": book.book_type})
+        return (json.dumps(res))
+    if request.method == 'DELETE':  # not implemented yet
+        del_book = Books.query.filter_by(id=bid).first()
+        db.session.delete(del_book)
         db.session.commit()
-        return  {"animal":"deleted"}
-    if request.method == 'PUT': #not implemented yet
+        return {"book": "deleted"}
+    if request.method == 'PUT':
         request_data = request.get_json()
-        upd_animal = Animals.query.filter_by(id = aid).first()
-        # upd_animal = Animals.query.get(id)
-        # print(Animals.query.get(id))
-        upd_animal.animal = request_data['animal']
-        upd_animal.kind = request_data["kind"]
-        upd_animal.size = request_data["size"] 
+        upd_book = Books.query.filter_by(id=bid).first()
+        upd_book.book_name = request_data['book_name']
+        upd_book.author = request_data["author"]
+        upd_book.year_published = request_data["year_published"]
+        upd_book.book_type = request_data["book_type"]
         db.session.commit()
-        return  {"animal":"updated"}
-        
- 
+        return {"book": "updated"}
+
+
+
+######################## ---CUSTOMER CRUDE---  #############################################
+
+@app.route('/customer', methods=["POST", "GET"])
+@app.route('/customer/<cid>', methods=["DELETE", "PUT"])
+@cross_origin()
+def crude_customer(cid=0):
+    if request.method == 'POST':
+        request_data = request.get_json()
+        customer_name = request_data['customer_name']
+        City = request_data["City"]
+        Age = request_data["Age"]
+        customer_mode = request_data["customer_mode"]
+        newCustomer = Customers(customer_name, City, Age, customer_mode)
+        db.session.add(newCustomer)
+        db.session.commit()
+        return {"Cutomer": "created"}
+    if request.method == 'GET':
+        res = []
+        for cus in Customers.query.all():
+            res.append({"customer_name": cus.customer_name,"id": cus.id, "City": cus.City, "Age": cus.Age, "customer_mode": cus.customer_mode})
+        return (json.dumps(res))
+    if request.method == 'DELETE':  # not implemented yet customer_mode
+        del_customer = Customers.query.filter_by(id=cid).first()
+        db.session.delete(del_customer)
+        db.session.commit()
+        return {"Customer": "deleted"}
+    if request.method == 'PUT':
+        request_data = request.get_json()
+        upd_customer = Customers.query.filter_by(id=cid).first()
+        upd_customer.customer_name = request_data['customer_name']
+        upd_customer.City = request_data["City"]
+        upd_customer.Age = request_data["Age"]
+        upd_customer.customer_mode = request_data["customer_mode"]
+        db.session.commit()
+        return {"book": "updated"}
+
+######################## ---    LOANS CRUDE---  #############################################
+
+@app.route('/loans', methods=["POST", "GET"])
+@app.route('/loans/<lid>', methods=["DELETE", "PUT",'PATCH'])
+@cross_origin()
+def crude_loans(lid=0):
+    if request.method == 'POST':
+        request_data = request.get_json()
+        customer_id = request_data["customer_id"]
+        book_id = request_data["book_id"]
+        Loan_date = request_data["Loan_date"]
+        Return_date = request_data["Return_date"]
+        loan_moed = request_data["loan_moed"]
+        newLoan = Loans(customer_id, book_id, Loan_date, Return_date,loan_moed)
+        db.session.add(newLoan)
+        db.session.commit()
+        return {"Book":"Loaned Successfully"}
+    
+    if request.method == 'GET':
+        res = []
+        for loan,book in db.session.query(Loans,Books).join(Books).all():
+            res.append({"id":loan.id,"customer_id":loan.customer_id,"book_id":loan.book_id,"Loan_date":loan.Loan_date,"Return_date":loan.Return_date,"loan_moed":loan.loan_moed,"book_type":book.book_type})
+        return (json.dumps(res))
+    #customer_id, book_id, Loan_date, Return_date,loan_moed
+    if request.method == 'PUT':
+        request_data = request.get_json()
+        upd_loan = Loans.query.filter_by(id=lid).first()
+        upd_loan.loan_moed = request_data["loan_moed"]
+        db.session.commit()
+        return {"loan": "updated"}
+
 if __name__ == '__main__':
-    with app.app_context():db.create_all()
-    app.run(debug = True)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
---
+++ readme.md
@@ -0,0 +1,29 @@
+----------------- index page ---------------------
+The welcome page, from where you can go to any part of the site you want
+
+----------------- Books page ---------------------
+The books page.
+On this page you can add books. To add a book, you need to enter the book's details: book name, author's name, publication date and book type.
+After adding the books, click on the "Add new book to library" button.
+You can search for books by the name of the book in the search bar.
+You can delete a book with the "Delete Book" button.
+You can update a book by filling in the book details and the "Update Book" button at the location of the book you want to update.
+
+----------------- Customer page ---------------------
+The customer page.
+On this page you can add customers. To add a customer, you need to enter the customer details: customer name, customer city, customer age and customer status (is he active or inactive)
+After adding the details, click on the "Add new customer to libery database" button.
+You can search for active customers by the customer's name in the search bar. and inactive customers can be searched
+You can turn an active customer into an inactive customer by changing customer mode from on, you need to fill in "off" and then click Update in the location of the customer you want to make inactive
+
+----------------- Loans page ---------------------
+
+The book loans page.
+On this page you can add a book loan.
+To loan a book you need to fill in "customer id" and "book id"
+"Loan date", "Return date", "loan mode"
+Loans can be displayed according to different filters.
+Only loan in the "on" loan mode
+Only loan in "off" loan mode
+Only a loan in the "on" loan mode In a state of delayed return (the current date is greater than the return date and they are still in the on state)
+To return a book loan, you can go to the "loan mode" box and fill in "off" there, then press the "close loan" button at the location of the book you want to close
